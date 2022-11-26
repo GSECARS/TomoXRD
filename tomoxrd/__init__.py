0c5e51da9a115c717e72b8ec6bd8c0a0f34193c6
Christofanis Skordas <55315750+skordaschristofanis@users.noreply.github.com>
2022-11-26 09:18:51 -0600

    Changed static version

--- tomoxrd/__init__.py
+++ tomoxrd/__init__.py
@@ -33,7 +33,7 @@ __version__ = _version.get_versions()["version"]
 
 # Change to static version for executable versions in order to display the correct software title
 if __version__ == "0+unknown":
-    __version__ = "0.0.2"
+    __version__ = "0.0.3"
 
 # Application controller
 app = MainController()
